b0100b0eaa5f458160cee0bd6ad7f6e30f22fb1d
Mansi231 <mansigabani231@gmail.com>
2024-01-08 12:05:12 +0530

    project setup

---
+++ server/schema/Blog.js
@@ -0,0 +1,60 @@
+const { default: mongoose, Schema } = require("mongoose");
+
+const blogSchema = mongoose.Schema({
+    blog_id:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    title:{
+        type:String,
+        require:true
+    },
+    banner:{
+        type:String
+    },
+    des:{
+        type:String,
+        maxlength:200,
+    },
+    content:{
+        type:[],
+    },
+    author:{
+        type:Schema.Types.ObjectId,
+        require:true,
+        ref:'users'
+    },
+    activity:{
+        total_likes:{
+            type:Number,
+            default:0,
+        },
+        total_comments:{
+            type:Number,
+            default:0,
+        },
+        total_reads:{
+            type:Number,
+            default:0,
+        },
+        total_parent_comments:{
+            type:Number,
+            default:0,
+        }
+    },
+    comments:{
+        type:[Schema.Types.ObjectId],
+        ref:'comments'
+    },
+    draft:{
+        type:Boolean,
+        default:false
+    }
+},{
+    timestamps:{
+        createdAt:'publishedAt'
+    }
+})
+
+export default mongoose.model('blogs',blogSchema)
